50985920a0cac3d21769bbed4e7c7ae5a5c326ba
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-05-23 23:36:17 +0200

    comment unused items

--- resources/views/layouts/components/app-sidebar.blade.php
+++ resources/views/layouts/components/app-sidebar.blade.php
@@ -3,7 +3,7 @@
 					<aside class="app-sidebar">
 						<div class="main-sidebar-header active">
 							<a class="header-logo active" href="{{url('index')}}">
-								<img src="{{asset('assets/img/brand/logo.png')}}" class="main-logo  desktop-logo" alt="logo">
+								{{-- <img src="{{asset('assets/img/brand/logo.png')}}" class="main-logo  desktop-logo" alt="logo"> --}}
 								<img src="{{asset('assets/img/brand/logo-white.png')}}" class="main-logo  desktop-dark" alt="logo">
 								<img src="{{asset('assets/img/brand/favicon.png')}}" class="main-logo  mobile-logo" alt="logo">
 								<img src="{{asset('assets/img/brand/favicon-white.png')}}" class="main-logo  mobile-dark" alt="logo">
@@ -12,7 +12,7 @@
 						<div class="main-sidemenu">
 							<div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"/></svg></div>
 							<ul class="side-menu">
-								<li class="side-item side-item-category">Main</li>
+								{{-- <li class="side-item side-item-category">Main</li>
 								<li class="slide">
 									<a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);">
                                         <svg xmlns="http://www.w3.org/2000/svg"  class="side-menu__icon" width="24" height="24" viewBox="0 0 24 24"><path d="M3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707l-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13zm7 7v-5h4v5h-4zm2-15.586 6 6V15l.001 5H16v-5c0-1.103-.897-2-2-2h-4c-1.103 0-2 .897-2 2v5H6v-9.586l6-6z"/></svg>
@@ -21,8 +21,8 @@
 										<li class="side-menu__label1"><a href="javascript:void(0);">Dashboards</a></li>
 										<li><a class="slide-item" href="{{url('index')}}">Dashboard-1</a></li>
 									</ul>
-								</li>
-								<li class="side-item side-item-category">@lang('app.menu')</li>
+								</li> --}}
+								{{-- <li class="side-item side-item-category">@lang('app.menu')</li>
 
                                 <li class="slide">
                                     <a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);">
@@ -31,7 +31,7 @@
                                         <li class="side-menu__label1"><a href="javascript:void(0);">Utilities</a></li>
                                         <li><a class="slide-item" href="{{url('extras')}}">Extras</a></li>
                                     </ul>
-                                </li>
+                                </li> --}}
 
                                 <li class="side-item side-item-category">@lang('app.companies_receivers')</li>
 
@@ -47,13 +47,13 @@
 
 
 {{--                                dashboard settings--}}
-								<li class="slide">
+								{{-- <li class="slide">
 									<a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg"  class="side-menu__icon" width="24" height="24" viewBox="0 0 24 24"><path d="M22 7.999a1 1 0 0 0-.516-.874l-9.022-5a1.003 1.003 0 0 0-.968 0l-8.978 4.96a1 1 0 0 0-.003 1.748l9.022 5.04a.995.995 0 0 0 .973.001l8.978-5A1 1 0 0 0 22 7.999zm-9.977 3.855L5.06 7.965l6.917-3.822 6.964 3.859-6.918 3.852z"/><path d="M20.515 11.126 12 15.856l-8.515-4.73-.971 1.748 9 5a1 1 0 0 0 .971 0l9-5-.97-1.748z"/><path d="M20.515 15.126 12 19.856l-8.515-4.73-.971 1.748 9 5a1 1 0 0 0 .971 0l9-5-.97-1.748z"/></svg><span class="side-menu__label">@lang('app.settings')</span><i class="angle fe fe-chevron-right"></i></a>
 									<ul class="slide-menu">
 										<li class="side-menu__label1"><a href="javascript:void(0);">@lang('app.settings')</a></li>
                                         <li><a class="sub-side-menu__item" href="{{route('switcherpage')}}">@lang('app.dashboard_settings')</a></li>
 									</ul>
-								</li>
+								</li> --}}
 
 
 							</ul>

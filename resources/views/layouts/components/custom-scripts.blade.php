40d75295dd3580fa5cc8c5d0045afb5c7a14f316
eslam <elbadry693@gmail.com>
2023-06-09 17:47:03 +0300

    web edits

--- resources/views/layouts/components/custom-scripts.blade.php
+++ resources/views/layouts/components/custom-scripts.blade.php
@@ -32,25 +32,3 @@
 		<script src="{{asset('assets/js/exported.js')}}"></script>
 
         <script src="{{asset('assets/plugins/toastr/js/toastr.min.js')}}"></script>
-
-        <script>
-            toastr.options = {
-                "closeButton": true,
-                "debug": false,
-                "newestOnTop": true,
-                "progressBar": true,
-                "positionClass": "toast-top-right",
-                "preventDuplicates": true,
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "5000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            }
-            toastr.error("testttttttttttttttttt","error");
-
-        </script>
--- resources/views/layouts/dashboard/auth/login.blade.php
+++ resources/views/layouts/dashboard/auth/login.blade.php
@@ -5,21 +5,23 @@
 @endsection
 
 @section('class')
-
     <div class="bg-primary">
+@endsection
 
-        @endsection
-
-        @section('content')
+@section('content')
 
             <div class="page-single">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xl-5 col-lg-6 col-md-8 col-sm-8 col-xs-10 card-sigin-main mx-auto my-auto py-45 justify-content-center">
+                        <div
+                            class="col-xl-5 col-lg-6 col-md-8 col-sm-8 col-xs-10 card-sigin-main mx-auto my-auto py-45 justify-content-center">
                             <div class="card-sigin mt-5 mt-md-0">
                                 <!-- Demo content-->
                                 <div class="main-card-signin d-md-flex">
-                                    <div class="wd-100p"><div class="d-flex mb-4"><a href="{{route('home')}}"><img src="{{asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a></div>
+                                    <div class="wd-100p">
+                                        <div class="d-flex mb-4"><a href="{{route('home')}}"><img
+                                                    src="{{asset('assets/img/brand/favicon.png')}}"
+                                                    class="sign-favicon ht-40" alt="logo"></a></div>
                                         <div class="">
                                             <div class="main-signup-header">
                                                 <h2>Welcome back!</h2>
@@ -31,12 +33,18 @@
                                                                 <form action="{{route('login')}}" method="post">
                                                                     @csrf
                                                                     <div class="form-group">
-                                                                        <label>@lang('app.email_or_phone')</label> <input class="form-control" name="identifier" placeholder="@lang('app.email_or_phone')" type="text">
+                                                                        <label>@lang('app.email_or_phone')</label>
+                                                                        <input class="form-control" name="identifier"
+                                                                               placeholder="@lang('app.email_or_phone')"
+                                                                               type="text">
                                                                     </div>
                                                                     <div class="form-group">
-                                                                        <label>@lang('app.password')</label> <input name="password" class="form-control" placeholder="********" type="password">
+                                                                        <label>@lang('app.password')</label> <input
+                                                                            name="password" class="form-control"
+                                                                            placeholder="********" type="password">
                                                                     </div>
-                                                                    <button type="submit" class="btn btn-primary btn-block">@lang('app.sign_in')</button>
+                                                                    <button type="submit"
+                                                                            class="btn btn-primary btn-block">@lang('app.sign_in')</button>
                                                                 </form>
                                                             </div>
                                                         </div>
@@ -55,12 +63,5 @@
                     </div>
                 </div>
             </div>
-
-        @endsection
-
-        @section('scripts')
-
-            <!-- generate-otp js -->
-            <script src="{{asset('assets/js/generate-otp.js')}}"></script>
 
 @endsection

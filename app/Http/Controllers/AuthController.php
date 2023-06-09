39efc3ba52018b3fcf5c1d9c95581b23aad10b97
eslam <elbadry693@gmail.com>
2023-06-09 17:57:23 +0300

    web edits

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
+        dd($request->all());
         try {
             $this->authService->loginWithEmailOrPhone(identifier: $request->identifier, password: $request->password);
             $toast = [
